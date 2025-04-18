7041a3778efb6d6c324563c1d7f62f03f3921fb0
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-04-18 17:17:37 +0200

    JournalLayout y JounalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,16 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ipsa eius similique excepturi nesciunt officiis earum nemo porro facilis eum. Maiores necessitatibus, tenetur error recusandae explicabo iure. Sapiente, et laborum.</Typography>
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+      
+    </JournalLayout>
   )
 }
