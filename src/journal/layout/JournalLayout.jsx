51e70fdd3562ad3bbb3efcacfb4dbfafbe9e5db6
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-04-19 12:39:20 +0200

    SideBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material"
-import { NavBar } from "../components";
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
@@ -9,13 +9,13 @@ export const JournalLayout = ({ children }) => {
 
             <NavBar drawerWidth={ drawerWidth } />
 
-            {/* Sidebar drawerWidth */}
+            <SideBar drawerWidth={ drawerWidth } />
 
             <Box
                 component='main'
                 sx={{ flexGrow: 1, p: 3 }}
             >
-                {/* Toolbar */}
+                <Toolbar />
 
                 { children }
                 
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -6,7 +6,11 @@ import { JournalLayout } from "../layout/JournalLayout"
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ipsa eius similique excepturi nesciunt officiis earum nemo porro facilis eum. Maiores necessitatibus, tenetur error recusandae explicabo iure. Sapiente, et laborum.</Typography>
+      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur 
+        ipsa eius similique excepturi nesciunt officiis earum nemo porro facilis eum. 
+        Maiores necessitatibus, tenetur error recusandae explicabo iure. Sapiente, 
+        et laborum.
+      </Typography>
 
       {/* NothingSelected */}
       {/* NoteView */}
