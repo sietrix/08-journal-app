0566207b351a2b448cffbba3cdba3f5506ca024e
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-04-22 20:48:55 +0200

    Configurar el AuthSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { authSlice } from '../auth/auth';
 
 export const store = configureStore({
-  reducer: {
+    reducer: {
+        auth: authSlice.reducer
 
-  },
-  
+    },
+
 });
