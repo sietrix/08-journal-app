cc272490f2cad799c6fad38fc3f952867a536d0c
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-04-26 20:33:40 +0200

    Logout de Firebase

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,31 +1,14 @@
-import { useEffect } from "react"
-import { onAuthStateChanged } from "firebase/auth"
 import { Navigate, Route, Routes } from "react-router"
-import { useDispatch, useSelector } from "react-redux"
-import { FirebaseAuth } from "../firebase/config"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
 import { CheckingAuth } from "../ui"
-import { login, logout } from "../store/auth"
+import { useCheckAuth } from "../hooks"
 
 
 
 export const AppRouter = () => {
 
-    const { status } = useSelector( state => state.auth );
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-      onAuthStateChanged( FirebaseAuth, async( user ) => {
-        if( !user ) return dispatch( logout() );
-
-        const { uid, email, displayName, photoURL } = user;
-        dispatch( login({ uid, email, displayName, photoURL }));
-      })
-    
-     
-    }, [])
-    
+  const status = useCheckAuth();
 
     if( status === 'checking') {
         return <CheckingAuth />
