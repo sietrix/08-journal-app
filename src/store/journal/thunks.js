afe9997847772a9b24e3717347cfd0377b28b3da
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-04-29 20:11:11 +0200

    store/journal/thunks.js

---
+++ src/store/journal/thunks.js
@@ -0,0 +1,21 @@
+
+
+
+export const startNewNote = () => {
+    return async( dispatch ) => {
+
+
+        // uid
+
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        }
+
+        //! dispatch
+        // dispatch( newNote )
+        // dispatch( activarNote )
+
+    }
+}
