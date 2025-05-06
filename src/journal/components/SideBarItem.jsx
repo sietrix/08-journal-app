0014c4d4a12a9562a4592005aafe8bcb9a68bfe9
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-05-06 18:38:47 +0200

    Activar una nota

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,11 +1,20 @@
+import { useMemo } from "react"
+import { useDispatch } from "react-redux"
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
 import { TurnedInNot } from "@mui/icons-material"
-import { useMemo } from "react"
+import { setActiveNote } from "../../store/journal"
 
 
 
 
-export const SideBarItem = ({ title = '', body, id }) => {
+export const SideBarItem = ({ title = '', body, id, date, imageUrls = [] }) => {
+
+
+    const dispatch = useDispatch();
+
+    const onClickNote = () => {
+        dispatch( setActiveNote({ title, body, id, date, imageUrls }) );
+    }
 
     const newTitle = useMemo( () => {
         return title.length > 17
@@ -16,7 +25,7 @@ export const SideBarItem = ({ title = '', body, id }) => {
 
     return (
         <ListItem disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={ onClickNote }>
                 <ListItemIcon>
                     <TurnedInNot />
                 </ListItemIcon>
