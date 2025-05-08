8eda0b95190ef9025f7f1dd2ca1c16f769f0dab4
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-05-08 12:28:31 +0200

    SweetAlert2 integrado

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useMemo } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
-import { ImageGallery } from "../components"
-import { useForm } from "../../hooks/useForm"
-import { setActiveNote, startSaveNote } from "../../store/journal"
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { SaveOutlined } from "@mui/icons-material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import Swal from "sweetalert2";
+import 'sweetalert2/dist/sweetalert2.css';
+import { ImageGallery } from "../components";
+import { useForm } from "../../hooks/useForm";
+import { setActiveNote, startSaveNote } from "../../store/journal";
 
 
 
@@ -12,7 +14,7 @@ export const NoteView = () => {
 
     const dispatch = useDispatch();
 
-    const { active: note } = useSelector( state => state.journal );
+    const { active: note, messageSaved, isSaving } = useSelector( state => state.journal );
 
     const { body, title, date, onInputChange, formState } = useForm( note );
 
@@ -25,6 +27,13 @@ export const NoteView = () => {
     useEffect(() => {
         dispatch( setActiveNote( formState ) );
     }, [ formState ])
+
+    useEffect(() => {
+        if( messageSaved.length > 0 ) {
+            Swal.fire('Nota actualizada', messageSaved, 'success');
+        }
+    }, [ messageSaved ])
+    
 
 
     const onSaveNote = () => {
@@ -47,7 +56,8 @@ export const NoteView = () => {
             </Grid>
             
             <Grid>
-                <Button 
+                <Button
+                    disabled={ isSaving }
                     onClick={ onSaveNote }
                     color="primary" 
                     sx={{ padding: 2}}>
