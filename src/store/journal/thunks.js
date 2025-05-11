42c94097d4b1d23732e07ff8f6a3cd6e5e29b6ca
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-05-11 18:54:12 +0200

    Borrar una nota

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,6 @@
-import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { collection, deleteDoc, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, setPhotosToActiveNote, setSaving, updateNote } from "./";
+import { addNewEmptyNote, deleteNoteById, savingNewNote, setActiveNote, setNotes, setPhotosToActiveNote, setSaving, updateNote } from "./";
 import { fileUpload, loadNotes } from "../../helpers";
 
 
@@ -77,5 +77,20 @@ export const startUploadingFiles = ( files = [] ) => {
         const photosUrls = await Promise.all( fileUploadPromises );
 
         dispatch( setPhotosToActiveNote( photosUrls ) );
+    }
+}
+
+
+export const startDeletingNote = () => {
+    return async( dispatch, getState ) => {
+        const { uid } = getState().auth;
+        const { active: note} = getState().journal;
+
+        const docRef = doc( FirebaseDB, `${ uid }id-user-1/journal/notes/${ note.id }`);
+        await deleteDoc( docRef );
+
+        dispatch( deleteNoteById( note.id ) );
+
+
     }
 }
